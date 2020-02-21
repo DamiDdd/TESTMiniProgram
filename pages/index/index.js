cf13a545bde5f0474731f595be7910225655afcf
DamiDdd <834767813@qq.com>
2020-02-21 21:10:53 +0800

    image

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imagePath: ''
+  },
 
+  handleChooseAlbum(){
+    wx.chooseImage({
+      //记得用箭头函数
+      success: (res) => {
+        const path = res.tempFilePaths[0]
+        this.setData({
+          imagePath: path
+        })
+      },
+    })
+  },
+
+  handleImageLoad(){
+    console.log("finish")
   },
 
   /**
